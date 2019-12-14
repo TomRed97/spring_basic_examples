7070ac9d6343b2d23a569948a98d751cebabdb88
Tamara Aprikyan <tamara.aprikyan@synisys.com>
2019-12-14 16:37:38 +0400

    Spring Security basic example.

---
+++ security/src/main/java/com/example/security/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.example.security.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class HomeController {
+    private Map<String, LocalDateTime> usersLastAccess = new HashMap<>();
+
+    @GetMapping("/")
+    public String getCurrentUser(@AuthenticationPrincipal User user, Model model) {
+        String username = user.getUsername();
+        model.addAttribute("username", username);
+        model.addAttribute("lastAccess", usersLastAccess.get(username));
+        usersLastAccess.put(username, LocalDateTime.now());
+        return "homePage";
+    }
+
+}
